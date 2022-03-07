44fcb46a96cc194efc009e0bf6b4afaff806629c
makarstasia <makarshinaa1@gmail.com>
2022-03-07 21:14:01 +0300

    Initial Commit

---
+++ Tutors/TutorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorsApp.swift
+//  Tutors
+//
+//  Created by Anastasia on 07.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TutorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
